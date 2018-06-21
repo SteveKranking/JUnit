893ab9aeb99054d47391390d9d474d5d30e6be4f
Stephen Kranking <stephenkranking@yahoo.com>
2018-06-21 16:17:15 -0400

    Ran a few more tests

--- JUnitPractice/src/com/in28minutes/junit5/StringTest.java
+++ JUnitPractice/src/com/in28minutes/junit5/StringTest.java
@@ -2,16 +2,57 @@ package com.in28minutes.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
 
+	//Good method for establishing connection to a database
+	@BeforeAll 
+	static void beforeAll() {
+		System.out.println("initialize connection to database");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("Close connection to databse");
+	}
+	
+	@BeforeEach
+	void beforeEach(TestInfo info) {
+		System.out.println("Initialize Test Data for " + info.getDisplayName());
+	}
+	
+	@AfterEach
+	void afterEach(TestInfo info) {
+		System.out.println("Clean up Test Data for " + info.getDisplayName());
+	}
+	
 	@Test
 	void test() {
 		int actuallength = "ABCD".length();
 		int expectedlength = 4;
 		//Always expected vs actual
 		assertEquals(expectedlength, actuallength);
+	}
+	
+	@Test
+	@DisplayName("When length is null, throw an exception")
+	void string_exception() {
+		String str = null;
+		//We want to expect a null pointer exception
+		assertThrows(NullPointerException.class,
+				// lambda exception
+				() -> {
+					str.length();
+				}
+				
+				);
 	}
 	
 	@Test
